bdb7de1bb743575c162d2a16b82aff807baddd05
emilyzh2018 <emilyzh2018@gmail.com>
2024-01-07 13:12:01 -0500

    e

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import MinMaxScaler
 from flask_cors import CORS, cross_origin
 from flask import Response
 from flask.helpers import send_from_directory
-app = Flask(__name__,static_url_path='')
+app = Flask(__name__,static_folder = 'sp/build', static_url_path='/')
 CORS(app,origins='*')
 
 
@@ -17,6 +17,11 @@ CORS(app)
     response = jsonify({'message': 'Hello, Flask is running!'})
     response
     return response """
+@app.route('/')
+@cross_origin()
+def index():
+    return app.send_static_file('index.html')
+
 
 
 @app.route('/predict',methods =['POST'] )
@@ -40,11 +45,6 @@ def predict():
     #response= jsonify({"pee":"pee runs"})
     response
     return response
-
-@app.route('/')
-@cross_origin()
-def home():
-    return jsonify({"poo":"flask"})
 
 
 
